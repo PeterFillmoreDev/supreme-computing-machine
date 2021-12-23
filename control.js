1a70e4ebfac631e8ea8b47e617bdbae23217c86b
PeterFillmoreDev <peterfillmoredev@gmail.com>
2021-12-23 10:05:57 -0500

    add math functions

--- control.js
+++ control.js
@@ -3,15 +3,36 @@ let display = document.querySelectorAll('.disp');
 let btnN = document.querySelectorAll('.btnn');
 let btnO = document.querySelectorAll('.btno');
 let btnEquals = document.querySelector('#=btn')
-let btnBackspace = document.querySelector('bbtn')
-let btnClearAll = document.querySelector('cebtn')
+let btnBackspace = document.querySelector('#bbtn')
+let btnClearAll = document.querySelector('#cebtn')
 
-
-//string array where [1] is the active string and [is the inactive string]
+//
 let numActive = 0;
 let numInactive = 0;
 let stage = 1;
+let opFunction = "";
 
+//functions for the math
+
+function add(active, inactive){
+  return(inactive + active);
+};
+
+function subtract(active, inactive){
+  return(inactive - active);
+};
+
+function multiply(active, inactive){
+  return(inactive * active);
+};
+
+function divide(active, inactive){
+  return(inactive / active);
+};
+
+
+
+// events for number buttons
 btnN.forEach(numBtn => {
   numBtn.addEventListener('click', function(e) {
     if (numActive == '0'){
@@ -26,6 +47,7 @@ btnN.forEach(numBtn => {
 });
 
 
+//events for operator buttons
 btnO.forEach(opBtn =>{
   opBtn.addEventListener('click', function(e){
     if(stage == 1){
@@ -34,6 +56,16 @@ btnO.forEach(opBtn =>{
       stage = 2;
       display[1].textContent = numActive;
       display[0].textContent = numInactive;
+      opfunction = opBtn.id;
     }
   });
 });
+
+
+/*
+btnEquals.addEventListener(e => {
+  if (stage == 2){
+
+  }
+});
+*/
